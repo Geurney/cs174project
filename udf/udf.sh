af46cc97bdb32665cfc093dd3fcb771d5d0c9382
Geurney <Geurney@users.noreply.github.com>
2015-11-30 11:05:43 -0800

    Update udf.sh

--- udf/udf.sh
+++ udf/udf.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 make
 /etc/init.d/mysql restart > /dev/null
-mysql -u root --password="cs174\$" -e "DROP FUNCTION IF EXISTS SUM_HE; CREATE AGGREGATE FUNCTION SUM_HE RETURNS STRING SONAME 'udf.so';"
+echo "Creating Project Database and SUM_HE Function ..."
+mysql -u root --password="cs174\$" -e "DROP DATABASE IF EXISTS project; CREATE DATABASE project; USE project; CREATE TABLE Employees(id integer, age integer, salary TEXT NOT NULL, PRIMARY KEY (id)); DROP FUNCTION IF EXISTS SUM_HE; CREATE AGGREGATE FUNCTION SUM_HE RETURNS STRING SONAME 'udf.so';" &> /dev/null
